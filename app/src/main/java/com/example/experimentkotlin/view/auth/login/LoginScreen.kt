c8f322436b0cf510cc9985137ec6e2c07f64dd59
George Melo <georgemelo2003@gmail.com>
2026-03-05 17:44:37 -0500

    Pantalla login formato terminada

--- app/src/main/java/com/example/experimentkotlin/view/auth/login/LoginScreen.kt
+++ app/src/main/java/com/example/experimentkotlin/view/auth/login/LoginScreen.kt
@@ -4,21 +4,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -41,18 +51,61 @@ fun LoginScreen() {
 
         Column(
             Modifier
-                .padding(padding)
-                .fillMaxSize()
                 .background(Color.White)
+                .padding(padding)
+                .padding(horizontal = 20.dp)
+                .fillMaxSize(),
+
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Español (España)")
+            Text(
+                "Español (España)",
+                color = Color.Gray,
+                modifier = Modifier.padding(top = 22.dp)
+            )
+            Spacer(Modifier.weight(1f))
             Image(
-                modifier = Modifier.size(125.dp),
+                modifier = Modifier.size(72.dp),
                 painter = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = "ExperimentKotlin"
             )
-            TextField(value = email, onValueChange = {email = it})
-            TextField(value = password, onValueChange = {password = it})
+            Spacer(Modifier.weight(1.3f))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(30),
+                value = email,
+                label = {Text("Usuario, correo electronico o móvil")},
+                onValueChange = { email = it })
+            Spacer(Modifier.height(12.dp))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(30),
+                value = password,
+                label = {Text("Contraseña")},
+                onValueChange = { password = it })
+            Spacer(Modifier.height(8.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+                onClick = {}) {
+                Text(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                    text = "Iniciar sesión",
+                )
+            }
+            TextButton(onClick = { }) { Text("¿Has olvidado la contraseña?") }
+            Spacer(Modifier.weight(1f))
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {}) { Text("Crear cuenta nueva") }
+            Icon(
+                modifier = Modifier
+                    .width(60.dp)
+                    .padding(vertical = 24.dp),
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "Logo",
+                tint = Color.Black
+            )
 
         }
 
